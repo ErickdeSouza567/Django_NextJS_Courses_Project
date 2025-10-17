2a49ec37308473aec9985826ed218384d7bd0611
erickdesouza567 <erickdesouza567@gmail.com>
2025-10-17 15:55:10 -0300

    add pagination

--- core/settings.py
+++ core/settings.py
@@ -158,7 +158,13 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.orders.OrderingFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12
 }
 
 
